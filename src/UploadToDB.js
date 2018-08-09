6227fbbf1ea59e5c3ab19f4d858241d30d4d00f6
장혁_집 <reaper83@naver.com>
2018-08-10 02:09:01 +0900

    @ db connect port 추가 및 서버 테스트를 위한 serial 연결로 변경

--- src/UploadToDB.js
+++ src/UploadToDB.js
@@ -12,6 +12,7 @@ class UploadToDB {
   constructor() {
     const dbInfo = {
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PW,
       database: process.env.DB_DB,
