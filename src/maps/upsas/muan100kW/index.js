ee905cd26f4e0798a9ecdc83c2415d0c397dc39e
장혁_집 <reaper83@naver.com>
2019-07-22 01:42:55 +0900

    @ 수중 태양광 100kW 시나리오 1차 작성

--- src/maps/upsas/muan100kW/index.js
+++ src/maps/upsas/muan100kW/index.js
@@ -1,4 +1,12 @@
-const { BLOCK, NONE } = require('../../../../../default-intelligence').dcmConfigModel.nodeDataType;
+const {
+  dcmConfigModel: {
+    reqWrapCmdType: reqWCT,
+    reqWrapCmdFormat: reqWCF,
+    reqDeviceControlType: reqDCT,
+    goalDataRange: goalDR,
+    nodeDataType: { BLOCK, NONE },
+  },
+} = require('../../../../../default-intelligence');
 
 /**
  * @type {mDeviceMap}
@@ -3244,6 +3252,23 @@ const map = {
             trueNodeList: ['P_008'],
             falseNodeList: ['GV_113', 'GV_114'],
           },
+          {
+            destPlaceId: 'SEB_ONE',
+            destPlaceName: '수중 태양광 증발지 그룹 1',
+            trueNodeList: ['P_004', 'P_005', 'P_006', 'P_007', 'P_008'],
+            falseNodeList: [
+              'GV_105',
+              'GV_106',
+              'GV_107',
+              'GV_108',
+              'GV_109',
+              'GV_110',
+              'GV_111',
+              'GV_112',
+              'GV_113',
+              'GV_114',
+            ],
+          },
         ],
       },
       // 해주 3 > 수중 태양광 증발지 6, 7, 8
@@ -3264,6 +3289,12 @@ const map = {
             destPlaceId: 'SEB_8',
             trueNodeList: ['P_011'],
             falseNodeList: ['GV_119', 'GV_120'],
+          },
+          {
+            destPlaceId: 'SEB_TWO',
+            destPlaceName: '수중 태양광 증발지 그룹 2',
+            trueNodeList: ['P_009', 'P_010', 'P_011'],
+            falseNodeList: ['GV_115', 'GV_116', 'GV_117', 'GV_118', 'GV_119', 'GV_120'],
           },
         ],
       },
@@ -3286,6 +3317,51 @@ const map = {
             destPlaceId: 'NCB',
             trueNodeList: ['P_013'],
             falseNodeList: ['WD_007', 'WD_008'],
+          },
+        ],
+      },
+      // ************ 장소 그룹 ***************
+      // 수중 태양광 증발지 그룹 1 > 해주 2, 3, 바다
+      {
+        srcPlaceId: 'SEB_ONE',
+        srcPlaceName: '수중 태양광 증발지 그룹 1',
+        destList: [
+          {
+            destPlaceId: 'BW_2',
+            trueNodeList: ['GV_101', 'GV_105', 'GV_107', 'GV_109', 'GV_111', 'GV_113'],
+            falseNodeList: ['GV_104', 'GV_106', 'GV_108', 'GV_110', 'GV_112', 'GV_114'],
+          },
+          {
+            destPlaceId: 'BW_3',
+            trueNodeList: ['GV_102', 'GV_104', 'GV_105', 'GV_107', 'GV_109', 'GV_111', 'GV_113'],
+            falseNodeList: ['GV_101', 'GV_103', 'GV_106', 'GV_108', 'GV_110', 'GV_112', 'GV_114'],
+          },
+          {
+            destPlaceId: 'SEA',
+            trueNodeList: ['GV_106', 'GV_108', 'GV_110', 'GV_112', 'GV_114'],
+            falseNodeList: ['GV_105', 'GV_107', 'GV_109', 'GV_111', 'GV_113'],
+          },
+        ],
+      },
+      // 수중 태양광 증발지 그룹 2 > 해주 2, 3, 바다
+      {
+        srcPlaceId: 'SEB_TWO',
+        srcPlaceName: '수중 태양광 증발지 그룹 2',
+        destList: [
+          {
+            destPlaceId: 'BW_3',
+            trueNodeList: ['GV_102', 'GV_115', 'GV_117', 'GV_119'],
+            falseNodeList: ['GV_103', 'GV_104', 'GV_116', 'GV_118', 'GV_120'],
+          },
+          {
+            destPlaceId: 'BW_4',
+            trueNodeList: ['GV_103', 'GV_115', 'GV_117', 'GV_119'],
+            falseNodeList: ['GV_102', 'GV_104', 'GV_116', 'GV_118', 'GV_120'],
+          },
+          {
+            destPlaceId: 'SEA',
+            trueNodeList: ['GV_116', 'GV_118', 'GV_120'],
+            falseNodeList: ['GV_115', 'GV_117', 'GV_119'],
           },
         ],
       },
@@ -3336,6 +3412,382 @@ const map = {
           'P_011',
           'P_012',
           'P_013',
+        ],
+      },
+      {
+        cmdId: 'rainMode',
+        trueNodeList: [
+          'GV_106',
+          'GV_108',
+          'GV_110',
+          'GV_112',
+          'GV_114',
+          'GV_116',
+          'GV_118',
+          'GV_120',
+          'WD_001',
+          'WD_004',
+          'WD_008',
+        ],
+        falseNodeList: [
+          'GV_101',
+          'GV_102',
+          'GV_103',
+          'GV_104',
+          'GV_105',
+          'GV_107',
+          'GV_109',
+          'GV_111',
+          'GV_113',
+          'GV_115',
+          'GV_117',
+          'GV_119',
+          'WD_002',
+          'WD_003',
+          'WD_005',
+          'WD_006',
+          'WD_007',
+          'P_001',
+          'P_002',
+          'P_003',
+          'P_004',
+          'P_005',
+          'P_006',
+          'P_007',
+          'P_008',
+          'P_009',
+          'P_010',
+          'P_011',
+          'P_012',
+          'P_013',
+        ],
+      },
+    ],
+    scenarioCmdList: [
+      {
+        scenarioId: 'rainMode',
+        scenarioList: [
+          // 모든 장치 닫기
+          {
+            wrapCmdFormat: reqWCF.SET,
+            wrapCmdType: reqWCT.CONTROL,
+            setCmdId: 'closeAllDevice',
+          },
+          // 염수 대피
+          [
+            // 결정지 염수 이동
+            [
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                wrapCmdGoalInfo: {
+                  goalDataList: [
+                    {
+                      nodeId: 'WL_017',
+                      goalValue: 1,
+                      goalRange: goalDR.LOWER,
+                    },
+                  ],
+                },
+                flowSrcPlaceId: 'NCB',
+                flowDestPlaceId: 'BW_5',
+              },
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                flowSrcPlaceId: 'NCB',
+                flowDestPlaceId: 'SEA',
+              },
+            ],
+            // 수중 태양광 증발지 그룹 2 염수 이동
+            [
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                wrapCmdGoalInfo: {
+                  goalDataList: [
+                    {
+                      nodeId: 'WL_014',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                    {
+                      nodeId: 'WL_015',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                    {
+                      nodeId: 'WL_016',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                  ],
+                },
+                flowSrcPlaceId: 'SEB_TWO',
+                flowDestPlaceId: 'BW_3',
+              },
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                flowSrcPlaceId: 'SEB_TWO',
+                flowDestPlaceId: 'SEA',
+              },
+            ],
+            // 수중 태양광 증발지 그룹 1 염수 이동
+            [
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                wrapCmdGoalInfo: {
+                  goalDataList: [
+                    {
+                      nodeId: 'WL_009',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                    {
+                      nodeId: 'WL_010',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                    {
+                      nodeId: 'WL_011',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                    {
+                      nodeId: 'WL_012',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                    {
+                      nodeId: 'WL_013',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                  ],
+                },
+                flowSrcPlaceId: 'SEB_ONE',
+                flowDestPlaceId: 'BW_2',
+              },
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                flowSrcPlaceId: 'SEB_ONE',
+                flowDestPlaceId: 'SEA',
+              },
+            ],
+            // 일반 증발지 2 염수 이동
+            [
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                wrapCmdGoalInfo: {
+                  goalDataList: [
+                    {
+                      nodeId: 'WL_004',
+                      goalValue: 2,
+                      goalRange: goalDR.LOWER,
+                    },
+                  ],
+                },
+                flowSrcPlaceId: 'NEB_2',
+                flowDestPlaceId: 'BW_1',
+              },
+              {
+                wrapCmdFormat: reqWCF.FLOW,
+                wrapCmdType: reqWCT.CONTROL,
+                flowSrcPlaceId: 'NEB_2',
+                flowDestPlaceId: 'SEA',
+              },
+            ],
+            // 일반 증발지 1 염수 이동
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              flowSrcPlaceId: 'NEB_1',
+              flowDestPlaceId: 'SEA',
+            },
+          ],
+          // 바다로 ~
+          {
+            wrapCmdFormat: reqWCF.SET,
+            wrapCmdType: reqWCT.CONTROL,
+            setCmdId: 'rainMode',
+          },
+        ],
+      },
+      {
+        scenarioId: 'normalFlowScenario',
+        scenarioList: [
+          // 모든 장치 닫기
+          {
+            wrapCmdFormat: reqWCF.SET,
+            wrapCmdType: reqWCT.CONTROL,
+            setCmdId: 'closeAllDevice',
+          },
+          // 저수지 2 > 저수지 1 염수 이동
+          [
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'RV_2',
+              flowDestPlaceId: 'RV_1',
+            },
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.RESTORE,
+              flowSrcPlaceId: 'RV_2',
+              flowDestPlaceId: 'RV_1',
+            },
+          ],
+          // 저수지 1 > 일반 증발지 1, 2 염수 이동 및 염도 이동
+          [
+            // 저수지 1 > 일반 증발지 1, 2 염수 이동
+            {
+              wrapCmdFormat: reqWCF.SINGLE,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              singleNodeId: ['P_001', 'WD_002', 'WD_003'],
+              singleControlType: reqDCT.TRUE,
+            },
+            // 염도가 적정 수준에 오르기를 기다림
+            {
+              wrapCmdFormat: reqWCF.SINGLE,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              singleNodeId: ['P_001', 'WD_002', 'WD_003'],
+              singleControlType: reqDCT.FALSE,
+            },
+            // 염도에 의한 염수 이동
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'NEB_2',
+              flowDestPlaceId: 'BW_2',
+            },
+          ],
+          // 염도에 의한 수중 태양광 증발지 염수 이동
+          [
+            // 해주 2 > 수중 태양광 증발지 그룹 1 염수 이동
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'BW_2',
+              flowDestPlaceId: 'SEB_ONE',
+            },
+            // 수중 태양광 증발지 그룹 1의 염도 달성 대기
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.RESTORE,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'BW_2',
+              flowDestPlaceId: 'SEB_ONE',
+            },
+            // 염도 달성: 수중 태양광 증발지 그룹 1 > 해주 3
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'SEB_ONE',
+              flowDestPlaceId: 'BW_3',
+            },
+            // 염수 이동 완료
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.RESTORE,
+              flowSrcPlaceId: 'SEB_ONE',
+              flowDestPlaceId: 'BW_3',
+            },
+            // 해주 3 > 수중 태양광 증발지 그룹 2
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'BW_3',
+              flowDestPlaceId: 'SEB_TWO',
+            },
+            // 염도 달성 대기
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.RESTORE,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'BW_3',
+              flowDestPlaceId: 'SEB_TWO',
+            },
+            // 염도 달성: 수중태양광 증발지 그룹 2 > 해주 4
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'SEB_TWO',
+              flowDestPlaceId: 'BW_4',
+            },
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.RESTORE,
+              flowSrcPlaceId: 'SEB_TWO',
+              flowDestPlaceId: 'BW_4',
+            },
+            // 해주 4 > 결정지 해주로 이동
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'BW_4',
+              flowDestPlaceId: 'NCB',
+            },
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.RESTORE,
+              flowSrcPlaceId: 'BW_4',
+              flowDestPlaceId: 'NCB',
+            },
+          ],
+          // 결정지 소금 생산
+          [
+            // 해주 5 > 결정지 염수 이동
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.CONTROL,
+              wrapCmdGoalInfo: {
+                limitTimeSec: 20,
+              },
+              flowSrcPlaceId: 'BW_5',
+              flowDestPlaceId: 'NCB',
+            },
+            {
+              wrapCmdFormat: reqWCF.FLOW,
+              wrapCmdType: reqWCT.RESTORE,
+              flowSrcPlaceId: 'BW_5',
+              flowDestPlaceId: 'NCB',
+            },
+          ],
         ],
       },
     ],
